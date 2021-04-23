7f648da9491074549bf01e9e6c2217aa63adb8d8
Christian Wagner <christian@Christians-MacBook-Pro-2.local>
2021-04-23 18:09:08 -0400

    login update commit

--- public_html/CS530_Entry.php
+++ public_html/CS530_Entry.php
@@ -6,9 +6,11 @@
     Date Modified: 04/22/2021
 */
 
-// TODO: This will be the entry point into the website
+// This is the main entry point into the website
 
-// go to main login handler if no POST vars are found
-if(!isset($_POST)) { header('location:Login_hdlr.php'); }
+// go to main login handler if no POST vars are found, else forward to main handler
+if(!isset($_SESSION)) {header('location:Login_hdlr.php');}
+else if(isset($_POST)) {/* Deal with vars */ header('location:Main_hdlr.php');}
+else {header('location:Main_hdlr.php');}
 
 ?>
--- public_html/Login_hdlr.php
+++ public_html/Login_hdlr.php
@@ -8,6 +8,6 @@
 
 // TODO: This will be the entry point for the login page, if no information forward to login page.
 
-header('location:Login.htm');
+header('location:Login.php');
 
 ?>
